c3c46d23f32b1d876e9e0d03f8782eeda716fae4
Christian Sigcha <127263263+CrisDavidSD@users.noreply.github.com>
2026-04-18 19:24:26 -0500

    Interfaz de login
    Formulario visual de acceso integrado.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sistema de Tickets de Incidencias Técnicas</h1>
+    <h2>Login</h2>
+    <form action="acceso.php" method="POST">
+        <fieldset>
+            <p>Usuario*: </p> 
+            <input type="text" name="nombre" id="nombre">
+            <p>Contraseña*: </p>
+            <input type="password" name="clave" id="clave">
+            <br><br>
+            <input type="submit" value="Enviar">
+        </fieldset>
+    </form>
+</body>
+</html>
